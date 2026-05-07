d382765a2c92208108f5ab8a7c8eb0445438491c
Manuel Fernández <90277976+manuelfernandezdg@users.noreply.github.com>
2026-05-07 08:58:34 -0300

    Clase 8

--- js/clase6.js
+++ js/clase6.js
@@ -1,9 +1,8 @@
-// document es el objeto, el getElementById() es el método, la propiedad textContent
-
 const nombre = "Manuel";
 const apellido = "Fernández";
 const ciudad = "Berazategui";
 
+// document es el objeto, el getElementById() es el método, la propiedad textContent
 document.getElementById('nombre').textContent = nombre + ' ' + apellido;
 
 document.getElementById('ciudad').textContent = ciudad;
